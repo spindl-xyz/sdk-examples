3745fc0c80d5f9930d4c149a9004370339539655
Meir Radnovich <meir.radnovich@toptal.com>
2023-11-30 20:59:17 +0200

    Adding AppsFlyer to demo

--- ios-sdk-example/Spindl SDK Demo/Spindl SDK Demo/DemoViewModel.swift
+++ ios-sdk-example/Spindl SDK Demo/Spindl SDK Demo/DemoViewModel.swift
@@ -11,15 +11,6 @@ import SwiftUI
 class DemoViewModel : ObservableObject {
     @Published var errorMessage = ""
     
-//    private func randomEvent() -> Event<[String:String]> {
-//        let dataName = String.random(of: 10)
-//        let props = [String.random(of: 9):String.random(of: 7)]
-//        let eventData = EventData(name: dataName, properties: props)
-//        let identity = EventIdentity(address: String.random(of: 22), customerUserId: "\(String.random(of: 6))@\(String.random(of: 9)).\(String.random(of: 3))".lowercased())
-//        let metadata = MobileSDKMetadata(persistentId: String.random(of: 12), ts: Date(timeIntervalSinceNow: Double.random(in: -340000000.0 ... -9.0)))
-//        return Event(type: .custom, data: eventData, identity: identity, metadata: metadata)
-//    }
-//    
     func trackRandomEvent() {
         Task {
             let eventProperties = ["property1":Int.random(in: -47...470), "property2":Int.random(in: 200...7777)]
@@ -30,7 +21,6 @@ class DemoViewModel : ObservableObject {
                 let msg = "Couldn't initialise Spindl SDK: \(error.localizedDescription)"
                 print(msg)
                 errorMessage = msg
-                
             }
         }
     }
--- ios-sdk-example/Spindl SDK Demo/Spindl SDK Demo/Spindl_SDK_DemoApp.swift
+++ ios-sdk-example/Spindl SDK Demo/Spindl SDK Demo/Spindl_SDK_DemoApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import SpindlSDK
-
+import AppsFlyerLib
 
 @main
 struct Spindl_SDK_DemoApp: App {
@@ -15,12 +15,21 @@ struct Spindl_SDK_DemoApp: App {
     @StateObject var demoViewModel = DemoViewModel()
     
     init() {
+        AppsFlyerLib.shared().appleAppID = "xyz.spindl.Spindl-SDK-Demo"
         Spindl.initialize(apiKey: "9662ea01-769c-4a5b-8126-9e2647493846")
         Task {
             do {
                 try await Spindl.shared.identify(customerUserId: "sdkdemo@spindl.xyz")
             } catch {
                 print("Couldn't initialise Spindl SDK: \(error.localizedDescription)")
+            }
+        }
+        
+        AppsFlyerLib.shared().start { (dict, err) in
+            if let e = err {
+                print("AppsFlyer error: \(e.localizedDescription)")
+            } else if let dict = dict {
+                print("AppsFlyer started: \(dict)")
             }
         }
     }
